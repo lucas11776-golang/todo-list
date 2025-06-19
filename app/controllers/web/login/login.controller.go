d8961d5d81bbbf045d570ca9a68d67be0d9ef0ea
lucas11776 <lucas11776>
2025-06-19 11:58:51 +0200

    Added: implemented login feature

--- app/controllers/web/login/login.controller.go
+++ app/controllers/web/login/login.controller.go
@@ -1,15 +1,28 @@
 package login
 
 import (
+	"server/app/services/authentication"
+
 	"github.com/lucas11776-golang/http"
 )
 
-// Login  View
+// Login Page
 func Index(req *http.Request, res *http.Response) *http.Response {
 	return res.View("authentication.login", http.ViewData{})
 }
 
 // Login User
 func Store(req *http.Request, res *http.Response) *http.Response {
-	return res
+	user, err := authentication.Login(
+		req.Validator.Value("email"),
+		req.Validator.Value("password"),
+	)
+
+	if err != nil {
+		return res.Back().WithError("login_error", err.Error())
+	}
+
+	res.Session.Set("user_id", user.ID)
+
+	return res.Redirect("todos")
 }
